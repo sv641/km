6ce55f691507333fb0f0b4bd7facf2fcf78c0de1
John Muth <muth@kontain.app>
2021-09-23 22:03:08 -0700

    update7

--- cloud/k8s/deploy/tests/test.sh
+++ cloud/k8s/deploy/tests/test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -x
+set -e
+
 pod=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -A -l kontain=test-app)
 
-kubectl exec -it $(pod)-- uname -r
+kubectl exec -it ${pod} -- uname -r
