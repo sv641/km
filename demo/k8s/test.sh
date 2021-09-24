971084b49a3b8ce01685f483258dc2b9dc4c6fc1
John Muth <muth@kontain.app>
2021-09-24 13:18:42 -0700

    check cluster state only

--- demo/k8s/test.sh
+++ demo/k8s/test.sh
@@ -3,22 +3,24 @@
 set -x
 set -e
 
-kubectl apply -f demo/k8s/test.yaml 
+kubectl get pod -A
 
-sleep 5
-
-kubectl describe deployments.apps -A
-
-kubectl get pod -A --show-labels
-
-minikube logs
-
-# Wait for test pod to be ready.
-kubectl wait --for=condition=ready pod -l kontain=test-app
-
-# Get name of test pod
-pod=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -A -l kontain=test-app)
-
-kubectl exec -it ${pod} -- uname -r
-
-kubectl delete -f demo/k8s/test.yaml 
+#kubectl apply -f demo/k8s/test.yaml 
+#
+#sleep 5
+#
+#kubectl describe deployments.apps -A
+#
+#kubectl get pod -A --show-labels
+#
+#minikube logs
+#
+## Wait for test pod to be ready.
+#kubectl wait --for=condition=ready pod -l kontain=test-app
+#
+## Get name of test pod
+#pod=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -A -l kontain=test-app)
+#
+#kubectl exec -it ${pod} -- uname -r
+#
+#kubectl delete -f demo/k8s/test.yaml 
