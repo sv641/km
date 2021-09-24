058e78fbc7279cad1f3f349b90d287a84f237cf4
John Muth <muth@kontain.app>
2021-09-23 22:30:02 -0700

    update8

--- cloud/k8s/deploy/tests/test.sh
+++ cloud/k8s/deploy/tests/test.sh
@@ -3,6 +3,10 @@
 set -x
 set -e
 
+# Wait for test pod to be ready.
+kubectl wait --for=condition=ready pod -l kontain=test-app
+
+# Get name of test pod
 pod=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -A -l kontain=test-app)
 
 kubectl exec -it ${pod} -- uname -r
