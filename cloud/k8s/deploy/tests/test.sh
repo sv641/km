3f1d8f5ac8a8ed574cba845b3cdd8897d3bfb2c7
John Muth <muth@kontain.app>
2021-09-23 23:05:49 -0700

    commit9

--- cloud/k8s/deploy/tests/test.sh
+++ cloud/k8s/deploy/tests/test.sh
@@ -3,6 +3,8 @@
 set -x
 set -e
 
+kubectl apply -f demo/k8s/test.yaml 
+
 # Wait for test pod to be ready.
 kubectl wait --for=condition=ready pod -l kontain=test-app
 
@@ -10,3 +12,5 @@ kubectl wait --for=condition=ready pod -l kontain=test-app
 pod=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -A -l kontain=test-app)
 
 kubectl exec -it ${pod} -- uname -r
+
+kubectl delete -f demo/k8s/test.yaml 
