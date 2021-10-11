de78abed8f0efdbb5970807d5f97be3bd7d676c0
John Muth <muth@kontain.app>
2021-10-11 02:57:57 -0700

    Use 'none' minikube driver.

--- demo/k8s/test.sh
+++ demo/k8s/test.sh
@@ -5,22 +5,22 @@ set -e
 
 kubectl get pod -A
 
-#kubectl apply -f demo/k8s/test.yaml 
-#
-#sleep 5
-#
-#kubectl describe deployments.apps -A
-#
-#kubectl get pod -A --show-labels
-#
-#minikube logs
-#
-## Wait for test pod to be ready.
-#kubectl wait --for=condition=ready pod -l kontain=test-app
-#
-## Get name of test pod
-#pod=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -A -l kontain=test-app)
-#
-#kubectl exec -it ${pod} -- uname -r
-#
-#kubectl delete -f demo/k8s/test.yaml 
+kubectl apply -f demo/k8s/test.yaml 
+
+sleep 5
+
+kubectl describe deployments.apps -A
+
+kubectl get pod -A --show-labels
+
+minikube logs
+
+# Wait for test pod to be ready.
+kubectl wait --for=condition=ready pod -l kontain=test-app
+
+# Get name of test pod
+pod=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -A -l kontain=test-app)
+
+kubectl exec -it ${pod} -- uname -r
+
+kubectl delete -f demo/k8s/test.yaml 
